ed619758577973bfe063f1f975827b2ec1ebcd2f
Vanellope7 <614322375@qq.com>
2022-10-21 16:59:39 +0800

    initial

---
+++ 前端交互系统/fontend_dp/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+
+</template>
+
+<script>
+
+import axios from "axios";
+
+export default {
+  name: 'App',
+  components: {
+
+  },
+  mounted() {
+    axios({
+      url: 'http://127.0.0.1:8000/baseDp/',
+      method: 'post',
+      data: {
+        'mechanism': 'laplace',
+        'searchWay': 'sum',
+        'epsilon': 1.0
+      }
+    }).then((data) => {
+      console.log(data.data);
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
