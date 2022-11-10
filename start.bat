ccaad8e8e53294627cb9140ff1128f2ba1938f22
Vanellope7 <614322375@qq.com>
2022-11-10 21:07:26 +0800

    initial

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 chcp 65001
-start cmd /k "cd 前端交互系统/frontend_dp&&npm run serve
-start cmd /k "cd 后端差分系统/backendDp&&python manage.py runserver 0.0.0.0:8000"
+start cmd /k "cd frontend_dp&&npm run serve
+start cmd /k "cd backendDp&&python manage.py runserver 0.0.0.0:8000"
 start http://localhost:8081
--- 后端差分系统/backendDp/baseDp/views.py
+++
@@ -1,9 +0,0 @@
-import json
-
-from django.http import JsonResponse
-from tools.utils import getStatistics
-
-def listorders(request):
-    postData = json.loads(request.body)
-    o = getStatistics(postData)
-    return JsonResponse({'privateRes': o['privateRes'], 'res': o['res']})
