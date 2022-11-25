0c01d015cc35755f4d272a00ac5e0b5c9a0ba26c
Vanellope7 <614322375@qq.com>
2022-11-25 23:24:07 +0800

    initial

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 chcp 65001
 start cmd /k "cd frontendDp&&npm install&&npm run serve
-start cmd /k "cd backendDp&&python manage.py runserver 0.0.0.0:8000"
+start cmd /k "cd backendDp&&pip install -r requirements.txt&&python manage.py runserver 0.0.0.0:8000"
 start http://localhost:8081
